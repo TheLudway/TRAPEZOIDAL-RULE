acf389876de6f977bdc05e3d7467a5ed7de93577
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-02-12 16:32:17 -0500

    DOCUMENTATION AND SIGNATURE

--- TRAPEZOIDAL-EXCEL.py
+++ TRAPEZOIDAL-EXCEL.py
@@ -6,18 +6,27 @@ wb = load_workbook('SPEED.xlsx')
 ws = wb.active
 Resultado_suma = 0
 
-#RECORRIDO DE LAS CELDAS PARA LA MULTIPLICACIÓN POR (2) EN LA SUMA
+# In the range that is input all the cells get multiply by 2
 for row in range(3, 40981):
     for col in [2]:
         char = get_column_letter(col)
         Resultado_suma += (ws[char + str(row)].value) * 2
 
-#AGREGAR LOS PRIMEROS VALORES A LA SUMA INTERIOR
+# Sum to the result the first and last cells
 Resultado_suma += ws["B2"].value + ws["B40981"].value
 
-#OBTENER LOS LÍMITES DE INTEGRACIÓN Y CÀLCULAR DELTA DE X
+# Get integration limits and determinate delta x
 Delta_x = (ws["A40981"].value - ws["A2"].value) / 40980
 
 Resultado_final = (Resultado_suma * Delta_x) / 2
 print(Resultado_final)
 
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+"""
