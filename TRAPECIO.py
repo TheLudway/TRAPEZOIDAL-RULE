e181f2f651744d6ae0cbe11fcc31b118c8531c5e
TheLudway <70478991+TheLudway@users.noreply.github.com>
2023-02-12 16:30:59 -0500

    DOCUMENTATION AND SIGNATURE

--- TRAPECIO.py
+++ TRAPECIO.py
@@ -4,27 +4,27 @@ n = int(input("Ingrese el número de parejas ordenadas: " )) - 1 # n intervals
 b = float(input("Último límite de integración: " ))  # Input the last limit of integration (x-axis normally)
 a = float(input("Primer límite de integración: " ))  # Input the first limit of integration (x-axis nomalli)
 delta_x = (b-a)/n  # Delta x
-diccionario = {}  # In this dictionary would be all the 
-Resultado = 0  # Se asigna un resultado con una variable para poder manipular a futuro
+diccionario = {}  # In this dictionary would be all the couples
+Resultado = 0  # Future variable
 
-for i in range(n+1):  # Se pide que se repita la acción de pedir datos n número de veces
-    x = input("Valor x de la pareja ordenada: " ) # Se pide la coordenada x
-    try:     #Verificación del x para ver si es una fracción o no
+for i in range(n+1):  # get all the couples
+    x = input("Valor x de la pareja ordenada: " ) # x axis
+    try:     # See if x axis is a fraction
         if not x.isnumeric():
             x = fractions.Fraction(x)
     except ValueError:
         (x + "NO ES UN NÚMERO")
     doubleofx = float(x) 
-    y = input("Valor y de la pareja ordenada: " ) # Se pide la coordenada y
-    try:    #Verificación del y para ver si es una fracción o no
+    y = input("Valor y de la pareja ordenada: " ) # y axis
+    try:    # see if y is a fraction
         if not y.isnumeric():
             y = fractions.Fraction(y)
     except ValueError:
         (y + "NO ES UN NÚMERO")
     doubleofy = float(y) 
-    diccionario.update({doubleofx:doubleofy}) # x and y se almacenan en el diccionario
+    diccionario.update({doubleofx:doubleofy}) # Dictionary get updated by x and y
 
-#OPERACIÓN PARA LA REGLA DEL TRAPECIO
+# sum the first and last value of the dictionary, then quit that values and the values rest multiply by 2 and sum to the final result
 Resultado += diccionario.get(a)
 diccionario.pop(a)
 Resultado += diccionario.get(b)
@@ -33,3 +33,13 @@ for i in diccionario.values():
     Resultado += i*2
 Resultado *= delta_x/2
 print(Resultado)
+
+"""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣶⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣄⣀⡀⣠⣾⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⡇⠀⠀⠀⠀
+⠀⣶⣿⣦⣜⣿⣿⣿⡟⠻⣿⣿⣿⣿⣿⣿⣿⡿⢿⡏⣴⣺⣦⣙⣿⣷⣄⠀⠀⠀
+⠀⣯⡇⣻⣿⣿⣿⣿⣷⣾⣿⣬⣥⣭⣽⣿⣿⣧⣼⡇⣯⣇⣹⣿⣿⣿⣿⣧⠀⠀
+⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠸⣿⣿⣿⣿⣿⣿⣿⣷⠀
+"""
